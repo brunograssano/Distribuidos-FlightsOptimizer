e3e550f2b3894f5939ca76ce34d298a5da81d05f
Franco Batastini <fbatastini@fi.uba.ar>
2023-09-22 16:20:18 -0300

    [ADD] DynamicFilter & FlightRow - Lacks Tests

---
+++ filters/filter.go
@@ -0,0 +1,128 @@
+package filters
+
+import (
+	"DistribuidosTP1/data_structures"
+	"errors"
+	"reflect"
+)
+
+type Filter struct {
+	FilterInterface
+}
+
+func NewFilter() *Filter {
+	return &Filter{}
+}
+
+func (filter *Filter) Equals(row data_structures.FlightRow, valueOfCompare any, colName string) (bool, error) {
+	typeOfData := reflect.TypeOf(valueOfCompare)
+	switch typeOfData.Kind() {
+	case reflect.Int:
+		rowData, err := row.GetAsInt(colName)
+		if err != nil {
+			return false, err
+		}
+		compareCasted := valueOfCompare.(int)
+		return compareCasted == rowData, nil
+	case reflect.String:
+		rowData, err := row.GetAsString(colName)
+		if err != nil {
+			return false, err
+		}
+		compareCasted := valueOfCompare.(string)
+		return compareCasted == rowData, nil
+	case reflect.Float32:
+		rowData, err := row.GetAsFloat(colName)
+		if err != nil {
+			return false, err
+		}
+		compareCasted := valueOfCompare.(float32)
+		return compareCasted == rowData, nil
+	default:
+		return false, errors.New("type of comparison value is not valid")
+	}
+}
+
+func (filter *Filter) Greater(row data_structures.FlightRow, valueOfCompare any, colName string) (bool, error) {
+	typeOfData := reflect.TypeOf(valueOfCompare)
+	switch typeOfData.Kind() {
+	case reflect.Int:
+		rowData, err := row.GetAsInt(colName)
+		if err != nil {
+			return false, err
+		}
+		compareCasted := valueOfCompare.(int)
+		return compareCasted < rowData, nil
+	case reflect.String:
+		rowData, err := row.GetAsString(colName)
+		if err != nil {
+			return false, err
+		}
+		compareCasted := valueOfCompare.(string)
+		return compareCasted < rowData, nil
+	case reflect.Float32:
+		rowData, err := row.GetAsFloat(colName)
+		if err != nil {
+			return false, err
+		}
+		compareCasted := valueOfCompare.(float32)
+		return compareCasted < rowData, nil
+	default:
+		return false, errors.New("type of comparison value is not valid")
+	}
+}
+
+func (filter *Filter) Less(row data_structures.FlightRow, valueOfCompare any, colName string) (bool, error) {
+	typeOfData := reflect.TypeOf(valueOfCompare)
+	switch typeOfData.Kind() {
+	case reflect.Int:
+		rowData, err := row.GetAsInt(colName)
+		if err != nil {
+			return false, err
+		}
+		compareCasted := valueOfCompare.(int)
+		return compareCasted > rowData, nil
+	case reflect.String:
+		rowData, err := row.GetAsString(colName)
+		if err != nil {
+			return false, err
+		}
+		compareCasted := valueOfCompare.(string)
+		return compareCasted > rowData, nil
+	case reflect.Float32:
+		rowData, err := row.GetAsFloat(colName)
+		if err != nil {
+			return false, err
+		}
+		compareCasted := valueOfCompare.(float32)
+		return compareCasted > rowData, nil
+	default:
+		return false, errors.New("type of comparison value is not valid")
+	}
+}
+
+func (filter *Filter) GreaterOrEquals(row data_structures.FlightRow, valueOfCompare any, colName string) (bool, error) {
+	eq, err := filter.Equals(row, valueOfCompare, colName)
+	if err != nil {
+		return false, err
+	}
+	gr, err2 := filter.Greater(row, valueOfCompare, colName)
+	if err2 != nil {
+		return false, err2
+	}
+
+	return eq || gr, nil
+}
+
+func (filter *Filter) LessOrEquals(row data_structures.FlightRow, valueOfCompare any, colName string) (bool, error) {
+	eq, err := filter.Equals(row, valueOfCompare, colName)
+	if err != nil {
+		return false, err
+	}
+	less, err2 := filter.Less(row, valueOfCompare, colName)
+	if err2 != nil {
+		return false, err2
+	}
+
+	return eq || less, nil
+}
