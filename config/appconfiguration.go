62dad0fc85e1921c15f68df99031030e9767d98d
Bruno Grassano <bgrassano@fi.uba.ar>
2023-09-24 13:17:56 -0300

    Ref move config functions to common file

---
+++ config/appconfiguration.go
@@ -0,0 +1,81 @@
+package config
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"log"
+	"strings"
+)
+
+func initEnv() (*viper.Viper, error) {
+	v := viper.New()
+
+	// Configure viper to read env variables with the CLI_ prefix
+	v.AutomaticEnv()
+	v.SetEnvPrefix("cli")
+	// Use a replacer to replace env variables underscores with points. This let us
+	// use nested configurations in the config file and at the same time define
+	// env variables for the nested configurations
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	// Add env variables supported
+	v.BindEnv("id")
+	v.BindEnv("log", "level")
+	v.BindEnv("queue", "type")
+
+	// Try to read configuration from config file. If config file
+	// does not exist then ReadInConfig will fail but configuration
+	// can be loaded from the environment variables, so we shouldn't
+	// return an error in that case
+	v.SetConfigFile("./config.yaml")
+	if err := v.ReadInConfig(); err != nil {
+		fmt.Printf("Configuration could not be read from config file. Using env variables instead")
+	}
+
+	return v, nil
+}
+
+// initLogger Receives the log level to be set in logrus as a string. This method
+// parses the string and set the level to the logger. If the level string is not
+// valid an error is returned
+func initLogger(logLevel string) error {
+	level, err := logrus.ParseLevel(logLevel)
+	if err != nil {
+		return err
+	}
+
+	customFormatter := &logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   false,
+	}
+	logrus.SetFormatter(customFormatter)
+	logrus.SetLevel(level)
+	return nil
+}
+
+// printConfig Print all the configuration parameters of the program.
+// For debugging purposes only
+func printConfig(v *viper.Viper) {
+	logrus.Infof("action: config | result: success | id: %s | log_level: %s | queue type: %v",
+		v.GetString("id"),
+		v.GetString("log.level"),
+		v.GetString("queue.type"),
+	)
+}
+
+// InitConfig Configures the application arguments. It ends the program if it fails to load correctly
+func InitConfig() *viper.Viper {
+	v, err := initEnv()
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	if err := initLogger(v.GetString("log.level")); err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	// Print program config with debugging purposes
+	printConfig(v)
+	return v
+}
--- main.go
+++ main.go
@@ -1,82 +1,14 @@
 package main
 
 import (
+	"DistribuidosTP1/config"
 	"DistribuidosTP1/middleware"
 	"fmt"
-	"github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
 	"log"
-	"strings"
 )
 
-func InitConfig() (*viper.Viper, error) {
-	v := viper.New()
-
-	// Configure viper to read env variables with the CLI_ prefix
-	v.AutomaticEnv()
-	v.SetEnvPrefix("cli")
-	// Use a replacer to replace env variables underscores with points. This let us
-	// use nested configurations in the config file and at the same time define
-	// env variables for the nested configurations
-	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-
-	// Add env variables supported
-	v.BindEnv("id")
-	v.BindEnv("log", "level")
-	v.BindEnv("queue", "type")
-
-	// Try to read configuration from config file. If config file
-	// does not exist then ReadInConfig will fail but configuration
-	// can be loaded from the environment variables, so we shouldn't
-	// return an error in that case
-	v.SetConfigFile("./config.yaml")
-	if err := v.ReadInConfig(); err != nil {
-		fmt.Printf("Configuration could not be read from config file. Using env variables instead")
-	}
-
-	return v, nil
-}
-
-// InitLogger Receives the log level to be set in logrus as a string. This method
-// parses the string and set the level to the logger. If the level string is not
-// valid an error is returned
-func InitLogger(logLevel string) error {
-	level, err := logrus.ParseLevel(logLevel)
-	if err != nil {
-		return err
-	}
-
-	customFormatter := &logrus.TextFormatter{
-		TimestampFormat: "2006-01-02 15:04:05",
-		FullTimestamp:   false,
-	}
-	logrus.SetFormatter(customFormatter)
-	logrus.SetLevel(level)
-	return nil
-}
-
-// PrintConfig Print all the configuration parameters of the program.
-// For debugging purposes only
-func PrintConfig(v *viper.Viper) {
-	logrus.Infof("action: config | result: success | client_id: %s | | log_level: %s | queue type: %v",
-		v.GetString("id"),
-		v.GetString("log.level"),
-		v.GetString("queue.type"),
-	)
-}
-
 func main() {
-	v, err := InitConfig()
-	if err != nil {
-		log.Fatalf("%s", err)
-	}
-
-	if err := InitLogger(v.GetString("log.level")); err != nil {
-		log.Fatalf("%s", err)
-	}
-
-	// Print program config with debugging purposes
-	PrintConfig(v)
+	v := config.InitConfig()
 	qMiddleware := middleware.NewQueueMiddleware()
 	if v.GetString("queue.type") == "c" {
 		queue := qMiddleware.CreateConsumer("test", true)
