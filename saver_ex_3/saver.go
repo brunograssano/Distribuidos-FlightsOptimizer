c0758c99ca4a0ad206724e54a5bb395e8abdfead
Bruno Grassano <bgrassano@fi.uba.ar>
2023-10-08 11:15:13 -0300

    Ref in simple saver
    * Change to consumer protocol
    * Handle EOF message

--- saver_ex_3/saver.go
+++ saver_ex_3/saver.go
@@ -4,6 +4,7 @@ import (
 	dataStructures "github.com/brunograssano/Distribuidos-TP1/common/data_structures"
 	"github.com/brunograssano/Distribuidos-TP1/common/filemanager"
 	"github.com/brunograssano/Distribuidos-TP1/common/middleware"
+	"github.com/brunograssano/Distribuidos-TP1/common/protocol"
 	"github.com/brunograssano/Distribuidos-TP1/common/utils"
 	log "github.com/sirupsen/logrus"
 )
@@ -11,14 +12,14 @@ import (
 // SimpleSaver Structure that handles the final results
 type SimpleSaver struct {
 	c          *SaverConfig
-	consumer   middleware.ConsumerInterface
+	consumer   protocol.ConsumerProtocolInterface
 	serializer *dataStructures.Serializer
 	canSend    chan bool
 }
 
 // NewSimpleSaver Creates a new saver for the results
 func NewSimpleSaver(qMiddleware *middleware.QueueMiddleware, c *SaverConfig, serializer *dataStructures.Serializer, canSend chan bool) *SimpleSaver {
-	consumer := qMiddleware.CreateConsumer(c.InputQueueName, true)
+	consumer := protocol.NewConsumerQueueProtocolHandler(qMiddleware.CreateConsumer(c.InputQueueName, true))
 	return &SimpleSaver{c: c, consumer: consumer, serializer: serializer, canSend: canSend}
 }
 
@@ -30,20 +31,21 @@ func (s *SimpleSaver) SaveData() {
 			log.Infof("Exiting saver")
 			return
 		}
-		msgStruct := s.serializer.DeserializeMsg(msg)
+
 		writer, err := filemanager.NewFileWriter(s.c.OutputFileNames[0])
 		if err != nil {
+			log.Errorf("Error opening file, closing saver")
 			return
 		}
-		for _, row := range msgStruct.DynMaps {
-			// todo cambiar a EOF msg
-			if row.GetColumnCount() == 0 {
-				s.canSend <- true
-				close(s.canSend)
-				log.Infof("Received all results")
-				return
-			}
+		log.Debugf("Received message: %v", msg)
+		if msg.TypeMessage == dataStructures.EOFFlightRows {
+			s.canSend <- true
+			close(s.canSend)
+			log.Infof("Received all results")
+			return
+		}
 
+		for _, row := range msg.DynMaps {
 			line := s.serializer.SerializeToString(row)
 			err = writer.WriteLine(line)
 			if err != nil {
