c7d218e62433a39f438e91815566e7a0577c9136
Franco Batastini <fbatastini@fi.uba.ar>
2023-10-10 23:26:57 -0300

    [FIX] Logging error on qMiddleware when was nil

--- common/middleware/queuemiddleware.go
+++ common/middleware/queuemiddleware.go
@@ -38,7 +38,11 @@ func (qm *QueueMiddleware) CreateExchangeProducer(nameExchange string, routingKe
 
 func (qm *QueueMiddleware) Close() {
 	err := qm.channel.Close()
-	log.Errorf("QueueMiddleware | Error closing QueueMiddleware Channel | %v", err)
+	if err != nil {
+		log.Errorf("QueueMiddleware | Error closing QueueMiddleware Channel | %v", err)
+	}
 	err = qm.conn.Close()
-	log.Errorf("QueueMiddleware | Error closing QueueMiddleware Connection | %v", err)
+	if err != nil {
+		log.Errorf("QueueMiddleware | Error closing QueueMiddleware Connection | %v", err)
+	}
 }
